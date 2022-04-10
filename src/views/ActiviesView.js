3c78689cd18a1080a45405989ff22cfcadb22e9c
Sunny <solange.vayogroup@gmail.com>
2022-04-09 21:21:18 -0300

    add view activies

--- src/views/ActiviesView.js
+++ src/views/ActiviesView.js
@@ -44,9 +44,192 @@ export default function HomeView() {
                         <div className='text'>
                             <span className='title-border-blue'>ACTIVIDADES</span>
                             <br />
-                            <span className='title-bold-blue'>EN LAS QUE ESTOY SUSCRIPTO</span>
+                            <span className='title-bold-blue'>EN LAS QUE <img src={lineBlue} alt="" /> <br /> ESTOY SUSCRIPTO</span>
                         </div>
                     </h1>
+                    <div className="columns is-multiline is-centered">
+                        <article className="column is-4">
+                            <div className="cont">
+                            <figure className='image is-fluid'>
+                                <img src={certificate1} alt="" />
+                            </figure>
+
+
+                            <div className="is-flex is-flex-direction-column is-justify-content-space-between px-4 pb-4 bg-blue">
+                                <h4 className='subtitle is-5 white'>Nombre de la actividad</h4>
+                                <p className='white mb-3'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut omnis, enim inventore dolor nihil alias!</p>
+                                <p className="control date">
+                                    <span className='button yelow'><i class="fa-regular fa-calendar"></i> 10/04/22</span>
+
+                                    <span className='button yelow'><i class="fa-regular fa-clock"></i> 16:00hs</span>
+
+                                </p>
+                                <hr class="bg-gray mt-5"></hr>
+                                <div class="field is-flex is-justify-content-center">
+                                    <p class="control">
+                                        <button class="button blue">
+                                            Ver más
+                                        </button>
+                                    </p>
+                                </div>
+                            </div>
+
+                            </div>
+                        </article>
+                        <article className="column is-4">
+                            <div className="cont">
+                            <figure className='image is-fluid'>
+                                <img src={certificate2} alt="" />
+                            </figure>
+
+
+                            <div className="is-flex is-flex-direction-column is-justify-content-space-between px-4 pb-4 bg-blue">
+                                <h4 className='subtitle is-5 white'>Nombre de la actividad</h4>
+                                <p className='white mb-3'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut omnis, enim inventore dolor nihil alias!</p>
+                                <p className="control date">
+                                    <span className='button yelow'><i class="fa-regular fa-calendar"></i> 10/04/22</span>
+
+                                    <span className='button yelow'><i class="fa-regular fa-clock"></i> 16:00hs</span>
+
+                                </p>
+                                <hr class="bg-gray mt-5"></hr>
+                                <div class="field is-flex is-justify-content-center">
+                                    <p class="control">
+                                        <button class="button blue">
+                                            Ver más
+                                        </button>
+                                    </p>
+                                </div>
+                            </div>
+
+                            </div>
+                        </article>
+                        <article className="column is-4">
+                            <div className="cont">
+                            <figure className='image is-fluid'>
+                                <img src={certificate3} alt="" />
+                            </figure>
+
+
+                            <div className="is-flex is-flex-direction-column is-justify-content-space-between px-4 pb-4 bg-blue">
+                                <h4 className='subtitle is-5 white'>Nombre de la actividad</h4>
+                                <p className='white mb-3'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut omnis, enim inventore dolor nihil alias!</p>
+                                <p className="control date">
+                                    <span className='button yelow'><i class="fa-regular fa-calendar"></i> 10/04/22</span>
+
+                                    <span className='button yelow'><i class="fa-regular fa-clock"></i> 16:00hs</span>
+
+                                </p>
+                                <hr class="bg-gray mt-5"></hr>
+                                <div class="field is-flex is-justify-content-center">
+                                    <p class="control">
+                                        <button class="button blue">
+                                            Ver más
+                                        </button>
+                                    </p>
+                                </div>
+                            </div>
+
+                            </div>
+                        </article>
+                    </div>
+                   
+                </section>
+
+                <section id='sectionSecondary' className='section bg-skyBlue'>
+                    <h1 className="titleProp">
+                        <div className='text'>
+                            <span className='title-border-white'>ACTIVIDADES</span>
+                            <br />
+                            <span className='title-bold-white'>EN LAS QUE NO<br /> ESTOY SUSCRIPTO</span>
+                        </div>
+                    </h1>
+                    <div className="columns is-multiline is-centered">
+                        <article className="column is-4">
+                            <div className="cont">
+                            <figure className='image is-fluid'>
+                                <img src={certificate1} alt="" />
+                            </figure>
+
+
+                            <div className="is-flex is-flex-direction-column is-justify-content-space-between px-4 pb-4 bg-white">
+                                <h4 className='subtitle is-5 blue'>Nombre de la actividad</h4>
+                                <p className='blue mb-3'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut omnis, enim inventore dolor nihil alias!</p>
+                                <p className="control date">
+                                    <span className='button skyBlue'><i class="fa-regular fa-calendar"></i> 10/04/22</span>
+
+                                    <span className='button skyBlue'><i class="fa-regular fa-clock"></i> 16:00hs</span>
+
+                                </p>
+                                <hr class="bg-gray mt-5"></hr>
+                                <div class="field is-flex is-justify-content-center">
+                                    <p class="control">
+                                        <button class="button bg-blue white">
+                                            Inscribirme
+                                        </button>
+                                    </p>
+                                </div>
+                            </div>
+
+                            </div>
+                        </article>
+                        <article className="column is-4">
+                            <div className="cont">
+                            <figure className='image is-fluid'>
+                                <img src={certificate2} alt="" />
+                            </figure>
+
+
+                            <div className="is-flex is-flex-direction-column is-justify-content-space-between px-4 pb-4 bg-white">
+                                <h4 className='subtitle is-5 blue'>Nombre de la actividad</h4>
+                                <p className='blue mb-3'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut omnis, enim inventore dolor nihil alias!</p>
+                                <p className="control date">
+                                    <span className='button skyBlue'><i class="fa-regular fa-calendar"></i> 10/04/22</span>
+
+                                    <span className='button skyBlue'><i class="fa-regular fa-clock"></i> 16:00hs</span>
+
+                                </p>
+                                <hr class="bg-gray mt-5"></hr>
+                                <div class="field is-flex is-justify-content-center">
+                                    <p class="control">
+                                        <button class="button bg-blue white">
+                                            Inscribirme
+                                        </button>
+                                    </p>
+                                </div>
+                            </div>
+
+                            </div>
+                        </article>
+                        <article className="column is-4">
+                            <div className="cont">
+                            <figure className='image is-fluid'>
+                                <img src={certificate3} alt="" />
+                            </figure>
+
+
+                            <div className="is-flex is-flex-direction-column is-justify-content-space-between px-4 pb-4 bg-white">
+                                <h4 className='subtitle is-5 blue'>Nombre de la actividad</h4>
+                                <p className='blue mb-3'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut omnis, enim inventore dolor nihil alias!</p>
+                                <p className="control date">
+                                    <span className='button skyBlue'><i class="fa-regular fa-calendar"></i> 10/04/22</span>
+
+                                    <span className='button skyBlue'><i class="fa-regular fa-clock"></i> 16:00hs</span>
+
+                                </p>
+                                <hr class="bg-gray mt-5"></hr>
+                                <div class="field is-flex is-justify-content-center">
+                                    <p class="control">
+                                        <button class="button bg-blue white">
+                                            Inscribirme
+                                        </button>
+                                    </p>
+                                </div>
+                            </div>
+
+                            </div>
+                        </article>
+                    </div>
                    
                 </section>
 
